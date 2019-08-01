99f3b38908a17112bb4776ed5272fa991f095956
WYC <147146890@qq.com>
2019-08-01 17:51:00 +0800

    登录页面提交

--- src/main/java/com/wtm/common/userInfo/controller/LoginController.java
+++ src/main/java/com/wtm/common/userInfo/controller/LoginController.java
@@ -1,11 +1,15 @@
 package com.wtm.common.userInfo.controller;
 
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 @Controller
@@ -66,6 +70,20 @@ public class LoginController {
         }
         map.put("msg", msg);
         // 此方法不处理登录成功,由shiro进行处理
+        return "/login";
+    }
+
+    /**
+     * 登出  这个方法没用到,用的是shiro默认的logout
+     * @param session
+     * @param model
+     * @return
+     */
+    @RequestMapping("/logout")
+    public String logout(HttpSession session, Model model) {
+        Subject subject = SecurityUtils.getSubject();
+        subject.logout();
+        model.addAttribute("msg","安全退出！");
         return "/login";
     }
 
