44b500e60e4d16e71c1fc502a99e6edf883d5392
WYC <147146890@qq.com>
2019-08-06 14:39:18 +0800

    登录页面提交

--- src/main/java/com/wtm/config/ShiroConfig.java
+++ src/main/java/com/wtm/config/ShiroConfig.java
@@ -20,7 +20,7 @@ public class ShiroConfig {
 
 		//拦截器.
 		Map<String,String> filterChainDefinitionMap = new LinkedHashMap<String,String>();
-		// 配置不会被拦截的链接 顺序判断
+		// 配置不会被拦截的链接 顺序判断//直接使用static会出现资源无法加载的问题
 		filterChainDefinitionMap.put("/statics/**", "anon");
 		//配置退出 过滤器,其中的具体的退出代码Shiro已经替我们实现了
 		filterChainDefinitionMap.put("/logout", "logout");
